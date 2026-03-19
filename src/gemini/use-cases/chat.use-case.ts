fce342e8651a04c6c3c3f0dda423030356492be7
MarcosGT23 <139613664+MarcosGT23@users.noreply.github.com>
2026-03-19 01:58:32 -0400

    Update chat.use-case.ts

--- src/gemini/use-cases/chat.use-case.ts
+++ src/gemini/use-cases/chat.use-case.ts
@@ -19,7 +19,7 @@ export const chatUseCase = async (
     `;
 
     const result = await ai.models.generateContent({
-            model: "gemini-1.5-flash",
+            model: "gemini-3-flash-preview",
         contents: [
             {
                 role: "user",
